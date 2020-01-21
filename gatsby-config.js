d05c45a2c89f77245b5db342e7a76a76d58cb71a
Axhon <axhonruiz@gmail.com>
2020-01-20 20:40:01 -0500

    boop

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Typescript Starter`,
+    title: `Axhon Ruiz-Gonzalez`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
     `gatsby-plugin-typescript`
-  ],
-}
+  ]
+};
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,29 +1,29 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import * as React from "react";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-import './index.css'
+import "./index.css";
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: "rebeccapurple",
+      marginBottom: "1.45rem"
     }}
   >
     <div
       style={{
-        margin: '0 auto',
+        margin: "0 auto",
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: "1.45rem 1.0875rem"
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            color: "white",
+            textDecoration: "none"
           }}
         >
           Gatsby
@@ -31,13 +31,13 @@ const Header = () => (
       </h1>
     </div>
   </div>
-)
+);
 
 interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
   location: {
-    pathname: string
-  }
-  children: any
+    pathname: string;
+  };
+  children: any;
 }
 
 class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
@@ -45,26 +45,32 @@ class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
     return (
       <div>
         <Helmet
-          title="Gatsby Default Starter"
+          title="Axhon Ruiz-Gonzalez"
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            {
+              name: "description",
+              content: "Axhon Ruiz-Gonzalez, Web Developer"
+            },
+            {
+              name: "keywords",
+              content: "web, developer, software, engineer, music, bass guitar"
+            }
           ]}
         />
         <Header />
         <div
           style={{
-            margin: '0 auto',
+            margin: "0 auto",
             maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
+            padding: "0px 1.0875rem 1.45rem",
+            paddingTop: 0
           }}
         >
           {this.props.children()}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default DefaultLayout
+export default DefaultLayout;
