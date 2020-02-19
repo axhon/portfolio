5cadbff9436e9dc988295be6e6cd34bb451249f4
Axhon Ruiz <axhonruiz@gmail.com>
2020-02-19 01:29:44 -0500

    linting

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,43 @@
-import * as React from 'react';
-import { Link, graphql } from 'gatsby';
-import Wrapper from '../components/Wrapper/Wrapper';
-import Nav from '../components/Nav/Nav';
+import * as React from 'react'
+import { Link, graphql } from 'gatsby'
+import Wrapper from '../components/Wrapper/Wrapper'
+import Nav from '../components/Nav/Nav'
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
 // to generate all types from graphQL schema
 interface IndexPageProps {
-  data: {
-    site: {
-      siteMetadata: {
-        title: string;
-      };
-    };
-  };
+    data: {
+        site: {
+            siteMetadata: {
+                title: string
+            }
+        }
+    }
 }
 
 const LandingPage: React.FC<IndexPageProps> = props => {
-  return (
-    <Wrapper>
-      <Nav />
-      <h1>Hi people</h1>
-      <p>
-        Welcome to your new{' '}
-        <strong>{props?.data?.site?.siteMetadata?.title}</strong> site.
-      </p>
-      <p>Now go build something great.</p>
-      <Link to="/page-2/">Go to page 2</Link>
-    </Wrapper>
-  );
-};
+    return (
+        <Wrapper>
+            <Nav />
+            <h1>Hi people</h1>
+            <p>
+                Welcome to your new{' '}
+                <strong>{props?.data?.site?.siteMetadata?.title}</strong> site.
+            </p>
+            <p>Now go build something great.</p>
+            <Link to="/page-2/">Go to page 2</Link>
+        </Wrapper>
+    )
+}
 
 export const pageQuery = graphql`
-  query IndexQuery {
-    site {
-      siteMetadata {
-        title
-      }
+    query IndexQuery {
+        site {
+            siteMetadata {
+                title
+            }
+        }
     }
-  }
-`;
+`
 
-export default LandingPage;
+export default LandingPage
