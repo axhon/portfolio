9a0aace9b221170c3a9f6661d31df820c49608c6
Axhon Ruiz <axhonruiz@gmail.com>
2020-02-19 00:49:20 -0500

    add declarations and typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ const LandingPage: React.FC<IndexPageProps> = props => {
       <h1>Hi people</h1>
       <p>
         Welcome to your new{' '}
-        <strong>{props.data.site.siteMetadata.title}</strong> site.
+        <strong>{props?.data?.site?.siteMetadata?.title}</strong> site.
       </p>
       <p>Now go build something great.</p>
       <Link to="/page-2/">Go to page 2</Link>
