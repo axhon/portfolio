40966eb62d01b34a3cd56c2aefdf218a0f0287f8
Axhon Ruiz <axhonruiz@gmail.com>
2020-02-23 18:46:03 -0500

    responsive about me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,29 +7,45 @@ const AboutMe: React.FC<{}> = () => {
         <section className="AboutMe">
             <h2>About</h2>
 
-            <h3>Let me introduce myself.</h3>
-            <img src={profile} className="Introduction-picture" />
-            <p>
-                I'm a Full-Stack Developer currently based on Oahu, HI.
-                Currently my focus is on web-based applications built with
-                Nodejs and Typescript. I also like to play music and perform on
-                stage.
-            </p>
+            <div className="abt">
+                <div className="abt-introduction">
+                    <h3>Let me introduce myself.</h3>
+                    <img src={profile} className="Introduction-picture" />
+                    <p>
+                        I&apos;m a Full-Stack Developer currently based on Oahu,
+                        HI. Currently my focus is on web-based applications
+                        built with Nodejs and Typescript. I also like to play
+                        music and perform on stage.
+                    </p>
+                </div>
 
-            <h3>Profile</h3>
-            <p>
-                Born in San Salvador, El Salvador, my family emigrated to
-                America while I was very young. Growing up, I was always
-                performing on stage as either a musician or actor. I took the
-                lessons I learned from these hobbies and use them to write more
-                empathic code.
-            </p>
+                <div className="abt-profile">
+                    <h3>Profile</h3>
+                    <p>
+                        Born in San Salvador, El Salvador, my family emigrated
+                        to America while I was very young. Growing up, I was
+                        always performing on stage as either a musician or
+                        actor. I took the lessons I learned from these hobbies
+                        and use them to write more empathic code.
+                    </p>
+                </div>
 
-            <h3>Skills</h3>
-            <p>
-                These are my top self-scored skills. You can find a more
-                accurate list of my skills in my resume, or ask me on twitter!
-            </p>
+                <div className="abt-skills">
+                    <h3>Skills</h3>
+                    <p>
+                        These are my top self-scored skills. You can find a more
+                        accurate list of my skills in my resume, or ask me on
+                        twitter!
+                    </p>
+                </div>
+
+                <a
+                    className="resume-btn abt-resume"
+                    href="https://resume.creddle.io/resume/i0ynygosl1u"
+                >
+                    Download Resume
+                </a>
+            </div>
         </section>
     )
 }
